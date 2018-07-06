5a48d390da2683abdbc5f5c8bddfb6efb881ddaa
snkoski <shawnnkoski@gmail.com>
2018-07-06 15:10:12 -0400

    add index and show to activities controller

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::ActivitiesController < ApplicationController
+  before_action :find_activity, only: [:show]
+    def index
+      @activities = Activity.all
+      render json: @activities
+    end
+
+    def show
+      @activity
+      render json: @activity
+    end
+
+    private
+
+    def activity_params
+
+    end
+
+    def find_activity
+      @activity = Activity.find(params[:id])
+    end
 end
