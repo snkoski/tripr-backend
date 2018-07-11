105f12a5890b8268e106736f8c399a53202aa5ff
snkoski <shawnnkoski@gmail.com>
2018-07-11 18:05:23 -0400

    change user.create to user.new

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,12 @@ before_action :find_user, only: [:update, :show, :destroy]
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: { errors: @user.errors.full_message }, status: :unprocessible_entity
+    end
   end
 
   def update
