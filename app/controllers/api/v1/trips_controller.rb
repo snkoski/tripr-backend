669c7a3aaa9605ed1770dcfde30065f1935b6b3c
snkoski <shawnnkoski@gmail.com>
2018-07-12 15:08:56 -0400

    trying to fix user destroy action

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -41,7 +41,8 @@ before_action :find_trip, only: [:update, :show, :destroy, :trip_activities]
     @trips = Trips.all
     @trip.destroy
     # flash[:success] = "Trip deleted"
-    render json: @trips
+    # render json: @trips
+    status: :accepted
   end
 
   def trip_activities
