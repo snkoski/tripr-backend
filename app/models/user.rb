2268401bc2054437c33f868c496dd1d4066658a5
snkoski <shawnnkoski@gmail.com>
2018-07-12 13:36:12 -0400

    validation back in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   has_many :trips, through: :user_trips
   has_many :destinations, through: :trips
   has_many :activities, through: :trips
-  # validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
-  # validates :username, presence: true, length: {maximum: 24 }, uniqueness: { case_sensitive: false }
-  # validates :password, presence: true, length: {minimum: 3}
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :username, presence: true, length: {maximum: 24 }
+  validates :password, presence: true, length: {minimum: 3}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
+
+### uniqueness not working when using heroku
+# , uniqueness: { case_sensitive: false }
