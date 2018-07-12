c335d08794b1708238d755c7c2e215308b995c73
snkoski <shawnnkoski@gmail.com>
2018-07-12 09:20:38 -0400

    uniqueness validations on username and email presence validations on username, email, password, first_name, and last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :trips, through: :user_trips
   has_many :destinations, through: :trips
   has_many :activities, through: :trips
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: {maximum: 24 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: {minimum: 3}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
